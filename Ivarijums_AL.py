af162e03e903b57c4f2d06c89f6f89af36ede755
Phenomaman1 <artemlevcenko74@gmail.com>
2026-03-04 14:32:22 +0200

    Pievientoas aprekinašana

--- Ivarijums_AL.py
+++ Ivarijums_AL.py
@@ -2,5 +2,7 @@ def Ievaddati():
     Absvars=float(input("Ievadi Abolu masu kg: "))
     Cukcena=float(input("Ievadi cukura cenu EUR: "))
     return Absvars Cukcena
-
+def Aprekin(Absvars, Cukcena):
+    Cuksvars=Absvars/2
+    Izmaksas=Cuksvars*Cukcena
 
